ef36cff62c78126af2334d5426a97f86250944a2
mattire <alr2d4@hotmail.com>
2026-04-24 13:04:25 +0300

    add files

---
+++ SimpleWpfPropertyGrid.Demo/Models/GeoLocation.cs
@@ -0,0 +1,8 @@
+namespace SimpleWpfPropertyGrid.Demo.Models;
+
+public class GeoLocation
+{
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public double AltitudeMeters { get; set; }
+}
